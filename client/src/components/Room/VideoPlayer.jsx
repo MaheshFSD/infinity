e487967cace9c83be6bdf9324546aa3c314f731b
MaheshFSD <maheshfsdeveloper@gmail.com>
2021-08-07 16:38:18 +0530

    videoplayer without styles

--- client/src/components/Room/VideoPlayer.jsx
+++ client/src/components/Room/VideoPlayer.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React,{ useContext } from 'react'
+import { SocketContext } from '../../context/Context';
 
 const VideoPlayer = () => {
+    const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } = useContext(SocketContext);
     return (
-        <div>
+        <>
+            {stream && (<h4>{name}</h4>)}
+            {stream &&  (<video  playsInline muted ref={myVideo} autoPlay />)}
             
-        </div>
+            {callAccepted && !callEnded && (<h4>{call.name}</h4>)}
+            {callAccepted && !callEnded && (                       
+            <video playsInline ref={userVideo} autoPlay />
+            )}
+        </>
     )
 }
 
