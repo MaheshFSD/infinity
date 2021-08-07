1273cf5af9196785b09cfeef69d5bf44e6826498
info-vivekranjan <75258584+info-vivekranjan@users.noreply.github.com>
2021-08-07 19:24:23 +0530

    HomePage,Register,Login for frontend Completed

--- client/src/Components/HomeNavBar.jsx
+++ client/src/Components/HomeNavBar.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './CssForHome/HomeNavBar.module.css';
+
 
 function HomeNav() {
 
     return (
-        <div>
+        <>
+            <div className={styles.homeLinkCont}>
+                <Link to="/" className={styles.homeNavLink1}>HomeLogo</Link>
 
+                <Link to="/login" className={styles.homeNavLink3}>Login</Link>
 
+                <Link to="/register" className={styles.homeNavLink2}>Register</Link>
+            </div>
 
-        </div>
+        </>
     )
 
 }
--- client/src/Components/MainHomePage.jsx
+++ client/src/Components/MainHomePage.jsx
@@ -1,11 +1,53 @@
 import React from 'react';
+import usersImg from '../Images/users.PNG';
+import styles from './CssForHome/MainHomePage.module.css';
+import { Link } from 'react-router-dom';
+
 
 function HomePage() {
 
     return (
-        <div>
+        <div className={styles.homePageMainCont}>
 
-            <h2>Connecting Developers For Solving Problems</h2>
+            <div className={styles.homePageCont}>
+
+                <div className={styles.homePageLeft}>
+                    <div className={styles.homeSlogn}>
+                        <div>Connecting Developers For Solving Problems</div>
+                    </div>
+
+                    <div className={styles.bottomLeft}>
+                        <div className={styles.signUpCont}>
+                            <div>
+                                For Existing Developers
+                            </div>
+                            <div>
+                                Put Your Email and Password for Login
+                            </div>
+
+                            <Link to="/login" className={styles.loginBtn}><span>Login</span></Link>
+                        </div>
+
+                        <div className={styles.loginCont}>
+                            <div>For New Developers</div>
+
+                            <div>Fill all the requires details to register </div>
+
+                            <Link to="/register" className={styles.registerBtn}><span>Register</span></Link>
+                        </div>
+                    </div>
+
+
+                </div>
+
+
+                <div className={styles.homePageRight}>
+                    <img src={usersImg} alt="Users" />
+                </div>
+
+            </div>
+
+
 
         </div>
     )
