799ff87bb0201b65c6b9e35454eca5807335d120
info-vivekranjan <75258584+info-vivekranjan@users.noreply.github.com>
2021-08-07 15:46:05 +0530

    Basic structure for Homepage

---
+++ client/src/Components/HomeNavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function HomeNav() {
+
+    return (
+        <div>
+
+
+
+        </div>
+    )
+
+}
+
+export { HomeNav }
---
+++ client/src/Components/MainHomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function HomePage() {
+
+    return (
+        <div>
+
+            <h2>Connecting Developers For Solving Problems</h2>
+
+        </div>
+    )
+
+}
+
+export { HomePage }
