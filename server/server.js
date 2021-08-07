cf237946ef086ee98c2bc01610f7b57a860a4725
MaheshFSD <maheshfsdeveloper@gmail.com>
2021-08-07 12:07:56 +0530

    server file created

--- server/server.js
+++ server/server.js
@@ -24,3 +24,9 @@ io.on("connection", (socket) => {
 	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
 		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
 	});
+    socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	});
+});
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
