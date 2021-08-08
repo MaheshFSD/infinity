93cde09a27f4e32ce19edc97c4bfcee1ae604632
info-vivekranjan <75258584+info-vivekranjan@users.noreply.github.com>
2021-08-08 09:37:56 +0530

    Install Dependencies for video chat

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,9 @@ import { Routes } from './components/Routes';
 function App() {
   return (
     <div className="App">
+      <Routes />
       <VideoPlayer />
       <VideoDetails />
-      <Routes />
     </div>
   );
 }
