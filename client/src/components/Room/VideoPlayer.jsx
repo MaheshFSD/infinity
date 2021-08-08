6e1d0c37951523e362053e5cbb7ba4d82c12dc31
MaheshFSD <maheshfsdeveloper@gmail.com>
2021-08-08 12:30:19 +0530

    reloading solved

--- client/src/components/Room/VideoPlayer.jsx
+++ client/src/components/Room/VideoPlayer.jsx
@@ -5,12 +5,10 @@ const VideoPlayer = () => {
     const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } = useContext(SocketContext);
     return (
         <>
-            {stream && (<h4>{name}</h4>)}
-            {stream &&  (<video  playsInline muted ref={myVideo} autoPlay />)}
+            {stream && (<div><h4>{name}</h4><video  playsInline muted ref={myVideo} autoPlay /></div>)}
             
-            {callAccepted && !callEnded && (<h4>{call.name}</h4>)}
-            {callAccepted && !callEnded && (                       
-            <video playsInline ref={userVideo} autoPlay />
+            {callAccepted && !callEnded && (<div><h4>{call.name}</h4>                      
+            <video playsInline ref={userVideo} autoPlay /></div>
             )}
         </>
     )
